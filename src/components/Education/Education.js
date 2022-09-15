094c04ddcf51610130e485be084d0bea9ed7f1ca
DivyaMaddipudi <divya.maddipudi1@gmail.com>
2022-09-14 22:08:50 -0700

    Portfolio

---
+++ src/components/Education/Education.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import uniqid from 'uniqid'
+import { education } from '../../portfolio'
+// import EducationContainer from '../EducationContainer/EducationContainer'
+import './Education.css'
+
+const Education = () => {
+  if (!education.length) return null
+
+  return (
+    <section id='education' className='section education'>
+      <h2 className='section__title'>Education</h2>
+      <ul className='eduction__list'>
+        {education.map((skill) => (
+          <li key={uniqid()} className='eduction__list-item btn btn--plain'>
+            <p>
+              <li>
+                <h3>{skill.name}</h3>
+              </li>
+            </p>
+            <p>
+              <li style={{ fontSize: '18px' }}>{skill.degree}</li>
+            </p>
+            <p>
+              <li style={{ fontSize: '18px' }}>Major: {skill.branch}</li>
+            </p>
+            <p>
+              <li style={{ fontSize: '18px' }}>CGPA: {skill.gpa}</li>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+export default Education
---
+++ src/portfolio.js
@@ -0,0 +1,102 @@
+const header = {
+  // homepage: 'https://',
+  // title: 'Satyadivya Maddipudi',
+}
+
+const about = {
+  name: 'Satyadivya Maddipudi',
+  role: 'Full Stack Developer',
+  description:
+    'Enthusiastic coder eager to contribute to team success through hard work, attention to detail, and excellent organizational skills. I thrive in environments where I am able to utilize my abilities in complex problem-solving to find solutions and achieve results.',
+  resume:
+    'https://drive.google.com/file/d/1-kXwbbGtnRxSe7RCIUUtVj28sexy25b5/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/satyadivya-maddipudi/',
+    github: 'https://github.com/DivyaMaddipudi',
+  },
+}
+
+const education = [
+  {
+    name: 'San Jose State University',
+    degree: "Master's Degree",
+    branch: 'Software Engineering',
+    gpa: '3.8',
+  },
+  {
+    name: 'Sathyabama Institute of Science and Technology',
+    degree: "Bachelor's Degree",
+    branch: 'Computer Science and Engineering',
+    gpa: '9.61',
+  },
+]
+
+const projects = [
+  {
+    name: 'StackOverflow Clone',
+    description:
+      'Designed an interactive Q/A system, integrated tags, answers, badges, and analytics dashboard for the questions and reputed users.',
+    stack: ['MongoDB', 'Express.js', 'React.js', 'Node.js'],
+    sourceCode: 'https://github.com/truptilokhande/CMPE273-Group12',
+  },
+  {
+    name: 'Simulation of Etsy',
+    description:
+      'Developed a clone of Etsy application using MERN stack that has primary functionalities with the persona of customer and shop owner to show shops and its items list, place order, and purchase history.',
+    stack: [
+      'MongoDB',
+      'Express.js',
+      'React.js',
+      'Node.js',
+      'MySQL',
+      'GraphQL',
+      'Kafka',
+      'Rdis',
+    ],
+    sourceCode: 'https://github.com/DivyaMaddipudi/Etsy_lab_2',
+  },
+  {
+    name: 'Expense Tracker',
+    description:
+      'Developed a web application which helps user to calculate and track their total monthly expenses.',
+    stack: ['Java', 'SpringBoot', 'Hibernate', 'Servlets', 'JSP'],
+    sourceCode: 'https://github.com/DivyaMaddipudi/ExpenseTracker',
+  },
+]
+
+const skills = [
+  'Apache Kafka',
+  'AWS Cloud (EC2, S3, RDS, ECR) ',
+  'C++',
+  'C Programming',
+  'CSS',
+  'Docker',
+  'Express.js',
+  'Git',
+  'GraphQL',
+  'Hibernate',
+  'HTML',
+  'Java',
+  'JavaScript',
+  'JIRA',
+  'JMeter',
+  'JUnit',
+  'Mocha',
+  'MongoDB',
+  'MySQL',
+  'Node.js',
+  'Passport.js',
+  'Python',
+  'React.js',
+  'Redis',
+  'Redux',
+  'Rest API',
+  'SpringBoot',
+]
+
+const contact = {
+  email: 'satyadivya.maddipudi@sjsu.edu',
+  phone: '+1 669-369-9841',
+}
+
+export { header, about, education, projects, skills, contact }
